9d50c5d0a24f01582d6551e00e90a1431c97c45b
yanggang <yang.gang@clussys.com>
2024-06-26 15:28:16 +0800

    modify binary tree implementation

--- binary_tree.c
+++ binary_tree.c
@@ -2,251 +2,232 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct node {
+#define MAX_STACK_SIZE   1024
+
+typedef struct node{
     int data;
-    struct node *left;
-    struct node *right;
+    struct node* left;
+    struct node* right;
 } Node;
 
-static void visit(Node* node)
-{
-    if(node) {
-        printf("%d ", node->data);
-        fflush(stdout);
-    }
-}
-
-Node* insert(Node* node, int data) {
-    if (node == NULL) {
-        Node* temp = (Node*)malloc(sizeof(Node));
-        temp->data = data;
-        temp->left = temp->right = NULL;
-        return temp;
-    }
- 
-    if (data < node->data)
-        node->left = insert(node->left, data);
-    else if (data > node->data)
-        node->right = insert(node->right, data);
- 
-    return node;
-}
- 
-void preorder(Node* node) {
-    if (node == NULL)
-        return;
- 
-    visit(node);
-    preorder(node->left);
-    preorder(node->right);
-}
- 
-void inorder(Node* node) {
-    if (node == NULL)
-        return;
- 
-    inorder(node->left);
-    visit(node);
-    inorder(node->right);
-}
- 
-void postorder(Node* node) {
-    if (node == NULL)
-        return;
- 
-    postorder(node->left);
-    postorder(node->right);
-    visit(node);
-}
- 
-Node* search(Node* node, int data) {
-    if (node == NULL)
-        return NULL;
- 
-    if (node->data == data)
-        return node;
- 
-    Node* temp = search(node->left, data);
-    if (temp != NULL)
-        return temp;
- 
-    return search(node->right, data);
-}
- 
-Node* minValueNode(Node* node) {
-    Node* current = node;
-    while (current->left != NULL)
-        current = current->left;
-    return current;
-}
- 
-Node* deleteNode(Node* root, int data) {
-    // step 1: Find the node
-    if (root == NULL)
-        return root;
- 
-    // If the node is found, delete it
-    if (data == root->data) {
-        // node with only one child or no child
-        if (root->left == NULL)
-            return root->right;
-        else if (root->right == NULL)
-            return root->left;
- 
-        // node with two children: Get the inorder successor (smallest in the right subtree)
-        Node* temp = minValueNode(root->right);
- 
-        // Copy the inorder successor's content to this node
-        root->data = temp->data;
- 
-        // Delete the inorder successor
-        root->right = deleteNode(root->right, temp->data);
-    } else if (data < root->data) {
-        root->left = deleteNode(root->left, data);
-    } else {
-        root->right = deleteNode(root->right, data);
-    }
- 
-    return root;
-}
-
-#define MAX_STACK_SIZE 1024
-
-typedef struct stack {
-    Node nodes[MAX_STACK_SIZE];
+typedef struct stack{
     int top;
+    Node* nodes[MAX_STACK_SIZE];
 } Stack;
 
-
-void InitStack(Stack* S)
+void init_stack(Stack* stack)
 {
-	S->top = -1;
-}
- 
-
-bool IsEmpty(Stack* S)
-{
-	if (S->top == -1)
-		return true;
-	else
-		return false;
-}
- 
-bool Push(Stack* S, Node* x)
-{
-	if (S->top == MAX_STACK_SIZE - 1)
-		return false;
-	S->nodes[++(S->top)] = *x;
-	return true;
-}
- 
-bool Pop(Stack* S, Node* x)
-{
-	if (S->top == -1)
-		return false;
-	*x = S->nodes[S->top];
-    S->nodes[S->top].data = 0;
-    S->top--;
-	return true;
+    stack->top = -1;
 }
 
-void preorder_non_recursive(Node* node) {
-    if (node == NULL)
+bool is_stack_full(Stack* stack)
+{
+    return (stack->top == MAX_STACK_SIZE - 1);
+}
+
+bool is_stack_empty(Stack* stack)
+{
+    return (stack->top == -1);
+}
+
+bool push(Stack* stack, Node* node)
+{
+    if(is_stack_full(stack)) 
+        return false;
+    stack->nodes[++stack->top] = node;
+    return true;
+}
+
+bool pop(Stack* stack, Node** node)
+{
+    if(is_stack_empty(stack)) 
+        return false;
+    *node = stack->nodes[stack->top--];
+    return true;
+}
+
+Node* top(Stack* stack)
+{
+    if(!is_stack_empty) return stack->nodes[stack->top];
+    return NULL;
+}
+
+Node* create_node(int data)
+{
+    Node* node = malloc(sizeof(Node));
+    if(!node) return NULL;
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void insert_node(Node* root, Node* left, Node* right)
+{
+    if(!root) return;
+    root->left = left;
+    root->right = right;
+}
+
+void visit(Node* node)
+{
+    if(!node) {
         return;
-    Stack S;
-    InitStack(&S);
-    Node* p_tmp = node, tmp;
-    while(p_tmp || (!IsEmpty(&S))) {
+    }
+    printf("%d ", node->data);
+    fflush(stdout);
+}
+ 
+void preorder_recursive(Node* root)
+{
+    if(!root) return;
+    visit(root);
+    preorder_recursive(root->left);
+    preorder_recursive(root->right);
+}
+
+void inorder_recursive(Node* root)
+{
+    if(!root) return;
+    inorder_recursive(root->left);
+    visit(root);
+    inorder_recursive(root->right);
+}
+
+void postorder_recursive(Node* root)
+{
+    if(!root) return;
+    postorder_recursive(root->left);
+    postorder_recursive(root->right);
+    visit(root);
+}
+
+
+void preorder_nonrecursive(Node* root)
+{
+    if(!root) return;
+    Stack stack;
+    init_stack(&stack);
+    Node* p_tmp = root;
+    while(p_tmp || (!is_stack_empty(&stack))) {
         if(p_tmp) {
             visit(p_tmp);
-            Push(&S, p_tmp);
-            p_tmp = p_tmp->left;
-        } else {
-            Pop(&S, &tmp);
-            p_tmp = tmp.right;
-        }
-    }
-}
-
-void inorder_non_recursive(Node* node)
-{
-    if (node == NULL)
-        return;
-    Stack S;
-    InitStack(&S);
-    Node* p_tmp = node, tmp;
-    while(p_tmp || (!IsEmpty(&S))) {
-        if(p_tmp) {
-            Push(&S, p_tmp);
-            p_tmp = p_tmp->left;
-        } else {
-            Pop(&S, &tmp);
-            visit(&tmp);
-            p_tmp = tmp.right;
-        }
-    }
-}
-
-void postorder_non_recursive(Node* node) {
-    if (node == NULL)
-        return;
-    Stack S;
-    InitStack(&S);
-    Node* p_tmp = node, tmp;
-    Node* test;
-    while(p_tmp || (!IsEmpty(&S))) {
-        if(p_tmp) {
-            Push(&S, p_tmp);
-            p_tmp = p_tmp->left;
-        } else {
-            Pop(&S, &tmp);
-            if(tmp.right && (test != &tmp)) {
-                Push(&S, &tmp);
-                test = &tmp;
-            } else {
-                visit(&tmp);
+            if(!push(&stack, p_tmp)) {
+                printf("[Error] stack full \r\n");
+                return;
             }
-            p_tmp = tmp.right;
+            p_tmp = p_tmp->left;
+        } else {
+            if(!pop(&stack, &p_tmp)) {
+                printf("[Error] stack empty \r\n");
+                return;
+            }
+            p_tmp = p_tmp->right;
+        }
+    }
+    return;
+}
+
+void inorder_nonrecursive(Node* root)
+{
+    if(!root) return;
+    Stack stack;
+    init_stack(&stack);
+    Node* p_tmp = root;
+    while(p_tmp || (!is_stack_empty(&stack))) {
+        if(p_tmp) {
+            if(!push(&stack, p_tmp)) {
+                printf("[Error] stack full \r\n");
+                return;
+            }
+            p_tmp = p_tmp->left;
+        } else {
+            if(!pop(&stack, &p_tmp)) {
+                printf("[Error] stack empty \r\n");
+                return;
+            }
+            visit(p_tmp);
+            p_tmp = p_tmp->right;
         }
     }
 }
 
-int main() 
+void postorder_nonrecursive(Node* root)
 {
-    Node* root = NULL;
- 
-    root = insert(root, 50);
-    root = insert(root, 30);
-    root = insert(root, 20);
-    root = insert(root, 40);
-    root = insert(root, 70);
-    root = insert(root, 60);
-    root = insert(root, 90);
-    root = insert(root, 10);
-    root = insert(root, 80);
-    root = insert(root, 120);
-    root = insert(root, 100);
-    root = insert(root, 110);
- 
-    printf("Preorder traversal of the created binary tree is: \n");
-    preorder(root);
- 
-    printf("\r\nInorder traversal of the created binary tree is: \n");
-    inorder(root);
-
-    printf("\r\nPostorder traversal of the created binary tree is: \n");
-    postorder(root);
-
-    printf("\r\n******************************************************* \r\n");
-
-    printf("Nonrecursive Preorder traversal of the created binary tree is \n");
-    preorder_non_recursive(root);
-
-    printf("\r\nNonrecursive Inorder traversal of the created binary tree is \n");
-    inorder_non_recursive(root);
-
-    printf("\r\nNonrecursive postorder traversal of the created binary tree is \n");
-    postorder_non_recursive(root);
- 
-    printf("\r\n");
+    if(!root) return;
+    Stack stack;
+    init_stack(&stack);
+    Node* p_tmp = root;
+    Node* popped_once = NULL;
+    while(p_tmp || (!is_stack_empty(&stack))) {
+        if(p_tmp) {
+            push(&stack, p_tmp);
+            p_tmp = p_tmp->left;
+        } else {
+            pop(&stack, &p_tmp);
+            // if current node has right child and its right child has never been accessed
+            // then we need to push this node into stack, and put current to its right child
+            if(p_tmp->right && (popped_once != p_tmp->right)) {
+                push(&stack, p_tmp);
+                p_tmp = p_tmp->right;
+            } else {
+                visit(p_tmp);
+                popped_once = p_tmp;
+                p_tmp = NULL;
+            }
+        }
+    }
 }
+
+
+int main()
+{
+    Node* root = create_node(0);
+    Node* node1 = create_node(1);
+    Node* node2 = create_node(2);
+    Node* node3 = create_node(3);
+    Node* node4 = create_node(4);
+    Node* node5 = create_node(5);
+    Node* node6 = create_node(6);
+    Node* node7 = create_node(7);
+    Node* node8 = create_node(8);
+    Node* node9 = create_node(9);
+    Node* node10 = create_node(10);
+    Node* node11 = create_node(11);
+    Node* node12 = create_node(12);
+    Node* node13 = create_node(13);
+    Node* node14 = create_node(14);
+
+    insert_node(root, node1, node2);
+    insert_node(node1, node3, node4);
+    insert_node(node2, node5, node6);
+    insert_node(node3, NULL, node7);
+    insert_node(node4, node8, NULL);
+    insert_node(node7, node9, node10);
+    insert_node(node10, node11, node12);
+    insert_node(node12, NULL, node13);
+    insert_node(node13, NULL, node14);
+
+    printf("********************** Recursive Traverse **********************\r\n");
+    printf("preorder recursive\r\n");
+    preorder_recursive(root);
+    printf("\r\n");
+    printf("inorder recursive\r\n");
+    inorder_recursive(root);
+    printf("\r\n");
+    printf("postorder recursive\r\n");
+    postorder_recursive(root);
+    printf("\r\n");
+
+    printf("********************** Non Recursive Traverse **********************\r\n");
+    printf("preorder nonrecursive\r\n");
+    preorder_nonrecursive(root);
+    printf("\r\n");
+
+    printf("inorder nonrecursive\r\n");
+    inorder_nonrecursive(root);
+    printf("\r\n");
+
+    printf("postorder nonrecursive\r\n");
+    postorder_nonrecursive(root);
+    printf("\r\n");
+}   
