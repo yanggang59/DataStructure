deab1a9f1ce44e6f45f4a521e4790614451d122b
yanggang <1449381582@qq.com>
2025-12-02 23:31:22 +0800

    add lee_code.c
    Signed-off-by: yanggang <1449381582@qq.com>

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ all:
 	gcc binary_tree.c -o binary_tree -g
 	gcc link_list.c -o link_list -g
 	gcc array.c -o array -g
+	gcc leet_code.c -o leet_code -g
+	gcc heap.c -o heap -g
 clean:
 	rm -f binary_tree
 	rm -f link_list
 	rm -f array
+	rm -f leet_code
+	rm -f heap
---
+++ leet_code.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int dump_array(int* array, int size)
+{
+    int i;
+    for(i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\r\n");
+}
+
+void bubble_sort_array(int* array, int size)
+{
+
+    int pos;
+    int tmp;
+    int i, j;
+
+    if(size <= 1) {
+        return;
+    }
+
+    for(i = 0; i < size ; i++) {
+        for(j = 0; j < size - 1 - i; j++) {
+            if(array[j] > array[j + 1]) {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+int* reverse_array(int* array, int size)
+{
+    int i;
+    int tmp;
+    for(i = 0; i < size / 2; i++) {
+        tmp = array[i];
+        array[i] = array[size - i - 1];
+        array[size - i - 1] = tmp;
+    }
+    return array;
+}
+
+int* create_array(int size, int step, int start)
+{
+    int i = 0;
+    int* array = malloc(size * sizeof(int));
+    for(i = 0; i < size; i++) {
+        array[i] = start + step * i + 1;
+    }
+    return array;
+}
+
+int* gen_random_array(int size)
+{
+    int i = 0;
+    int* array = malloc(size * sizeof(int));
+    for(i = 0; i < size; i++) {
+        array[i] = rand() % 100;
+    }
+    return array;
+}
+
+static int partition(int *array, int left, int right)
+{
+    int pivot = array[left];
+    while(right > left) {
+        while ((right > left) && array[right] >= pivot) right--;
+        array[left] = array[right];
+        while ((right > left) && array[left] <= pivot) left++;
+        array[right] = array[left]; 
+    }
+    array[left] = pivot;
+    return left;
+}
+
+static int qselect(int* array, int left, int right, int target_idx)
+{
+    int idx = partition(array, left, right);
+    if (idx == target_idx) return array[idx];
+    else if (idx < target_idx) return qselect(array, idx + 1, right, target_idx);
+    else return qselect(array, left, idx - 1, target_idx);
+}
+
+
+int find_k_th_largest(int *array, int size, int k)
+{
+    if((size < 1) || (k > size)) return -1;
+    return qselect(array, 0, size - 1, size - k);
+}
+
+
+/**
+* TOPK Problem
+*/
+void test_find_k_th_largest()
+{
+    int size = 20;
+    int* array;
+    int topk = 0;
+    
+    array = gen_random_array(size);
+    dump_array(array, size);
+
+    topk = find_k_th_largest(array, size, size - 4);
+
+    printf("Top %d largest = %d \r\n", 4, topk);
+
+    free(array);
+}
+
+
+int main()
+{
+    test_find_k_th_largest();
+
+    return 0;
+}
